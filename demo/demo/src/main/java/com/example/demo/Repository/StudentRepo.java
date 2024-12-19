676b992e718289884eeb076c0790b7360467bce0
Osama Amrawy <amrawyosama@gmail.com>
2024-12-19 03:09:06 +0200

    solving error

--- demo/demo/src/main/java/com/example/demo/Repository/StudentRepo.java
+++ demo/demo/src/main/java/com/example/demo/Repository/StudentRepo.java
@@ -1,9 +1,10 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepo extends JpaRepository {
+public interface StudentRepo extends JpaRepository<Student, Long> {
 
 }
