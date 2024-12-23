f4eeca9a599eec4517875a119073c61baa7505ce
Doola0501 <149879265+Doola0501@users.noreply.github.com>
2024-12-23 18:10:26 +0200

    Update AssignmentRepository.java

--- demo/demo/src/main/java/com/example/demo/Repository/AssignmentRepository.java
+++ demo/demo/src/main/java/com/example/demo/Repository/AssignmentRepository.java
@@ -4,8 +4,10 @@ package com.example.demo.Repository;
 import com.example.demo.Models.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     // You can define custom queries here if needed
+      List<Assignment> findByCourseIdIn(List<Long> courseIds);
 }
