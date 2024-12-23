e0bbca1fdb0568de630f436a49005c32a806366d
Doola0501 <149879265+Doola0501@users.noreply.github.com>
2024-12-23 18:09:34 +0200

    Update EnrollmentRepo.java

--- demo/demo/src/main/java/com/example/demo/Repository/EnrollmentRepo.java
+++ demo/demo/src/main/java/com/example/demo/Repository/EnrollmentRepo.java
@@ -4,10 +4,14 @@ import com.example.demo.Models.Course;
 import com.example.demo.Models.Enrollment;
 import com.example.demo.Models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface EnrollmentRepo extends JpaRepository<Enrollment, Long> {
     Enrollment findByStudentAndCourse(Student student, Course course);
+    @Query("SELECT e.course.id FROM Enrollment e WHERE e.student.id = :studentId")
+    List<Long> findCourseIdsByStudentId(@Param("studentId") Long studentId);
 }
