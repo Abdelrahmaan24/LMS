b5f7f5e55fb4ae9878dc8934ae9bbb00955c3915
Doola0501 <149879265+Doola0501@users.noreply.github.com>
2024-12-23 22:52:01 +0200

    Update AttendanceRepo.java

--- demo/demo/src/main/java/com/example/demo/Repository/AttendanceRepo.java
+++ demo/demo/src/main/java/com/example/demo/Repository/AttendanceRepo.java
@@ -3,8 +3,10 @@ package com.example.demo.Repository;
 import com.example.demo.Models.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AttendanceRepo extends JpaRepository<Attendance, Integer> {
-
+ List<Attendance> findByStudentId(Long studentId);
+    List<Attendance> findByLessonId(Long lessonId);
 }
